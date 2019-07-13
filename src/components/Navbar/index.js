1e88b98c12e45f179f9c746ce513d4932eb49ff4
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-07-13 12:56:23 -0400

    navbar working

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,1 @@
-import React from "react";
-import "./Navbar.css";
-
-function Navbar() {
-  return (
-    <nav className="navbar navbar-light bg-light">
-    <span className="navbar-brand mb-0 h1">Jimmy'z Mobile Bike Repair</span>
-  </nav>
-  );
-}
-
-export default Navbar;
+export {default} from "./Navbar";
