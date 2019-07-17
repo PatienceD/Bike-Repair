af020f3d8772a6cb27df1efeac9dd4431a0c3341
Patience D <patience.deaton2@outlook.com>
2019-07-17 00:13:49 -0400

    pulling changes

--- src/App.js
+++ src/App.js
@@ -3,33 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-<<<<<<< HEAD
-import Clients from "./pages/Clients";
-=======
->>>>>>> 01da2329c01b6324864f77fe926df1b63ae53e96
 
 import Calendly from "./pages/Appointments";
 import NoMatch from "./pages/NoMatch";
 
-<<<<<<< HEAD
-function App() {
-  return (
-    <div className="Header">
-      <Router>
-        <Navbar />
-        <Header />
-        {/* <Body /> */}
-        <Switch>
-          {/* <Route exact path="/" component={Appointments} /> */}
-                <Route exact path="/clients" component={Clients} />
-                {/* <Route exact path="/account" component={Account} />
-                <Route component={NoMatch} /> */}
-        </Switch>
-      </Router>
-    </div>
-  );
-}
-=======
     function App() {
         return (
         <div className ="Header">
@@ -49,5 +26,4 @@ function App() {
       }
       
       export default App;
->>>>>>> 01da2329c01b6324864f77fe926df1b63ae53e96
 
