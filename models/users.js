ea7e54dc12d95cc5fcbe7f0d7f68c452b09a94cb
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-07-24 19:09:21 -0400

    database structure in server.js

--- models/users.js
+++ models/users.js
@@ -1,20 +1,46 @@
 module.exports = function(sequelize, DataTypes) {
-    var Burger = sequelize.define("Burger", {
-      burger_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      devoured: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+  var User = sequelize.define("User", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        isNumberic: true
       }
-    }, {
-      classMethods: {
-        associate: function(models) {
-          Burger.hasOne(models.Customer);
-        }
+    },
+    email: {
+      type: DataTypes.EMAIL,
+      allowNull: false,
+      validate:{
+        isEmail: true
       }
-    });
-    return Burger;
-  };
-  
+    },
+    bikeType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        isIn:[["Mountain Bike", "Road Bike"]]
+      }
+    },
+    lastWork: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+    },
+    adult: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    },
+    notes: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+  return User;
+};
