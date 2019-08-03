e7e5817d5b92a4706204753d648120d66a45084c
Patience D <patience.deaton2@outlook.com>
2019-08-03 09:43:42 -0400

    pulling recent changes

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const db = require("./models");
-<<<<<<< HEAD
-const routes = require("./controllers/bike-controller.js");
-=======
 const routes = require("../Bike-Repair/controllers/bike-controller.js");
->>>>>>> d0d91e0b9cbda4d5a51692a58457c13198dca2a1
 const app = express();
 var mysql = require("mysql");
 const PORT = process.env.PORT || 8080;
