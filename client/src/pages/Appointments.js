578acc77cf8597bc523e7423672d1ee88e871fdf
Patience D <patience.deaton2@outlook.com>
2019-08-26 22:19:06 -0400

    added pictures and links to front page

--- client/src/pages/Appointments.js
+++ client/src/pages/Appointments.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Modal from "../Modal";
 import { StateContext } from "../State";
+import Bike from "./pic/bike.png";
+import Jimmy from "./pic/jimmy.jpg";
+import Truck from "./pic/truck.jpg";
+import Customer from "./pic/customer.png";
 
 
 class Calendly extends React.Component {
@@ -54,19 +58,38 @@ class Calendly extends React.Component {
         <Modal />
         {/* GOOGLE MODAL LOGIN */}
 
-        {/* <div className="container">
-          <h4>See your Appointment Information</h4>
-          <div className="card">
-            <div className="card-body"> */}
-        {/* <h5 className="card-title">Appointment Information</h5> */}
-        {/* <h6 className="card-text">With: Jimmy Deaton</h6>
-              <p className="card-text">Where: Williamsburg Starbucks</p>
-              <p className="card-text">When: 09/06/2019   11:00 AM EST</p> */}
-        {/* <button href="#" className="card-link">Reschedule</button> */}
-        {/* <a className="btn btn-blue" type="button" target="_blank" rel="noopener noreferrer" href="https://m.me/281457917012">Ask Jimmy a Question</a>
+        <div className="container">
+          <div className="row" style={{ marginRight: '35px'}}>
+            <div className="col-md-3">
+              <img src={Bike} style={{ height: '200px', width: '200px', border: '4px solid #9dc83e', boxShadow: '2px 2px 5px #000', margin: '7px' }}></img>
             </div>
+            <div className="col-md-4">
+              <img src={Jimmy} style={{ height: '200px', width: '300px', border: '4px solid #9dc83e', boxShadow: '2px 2px 5px #000', margin: '7px' }}></img>
+            </div>
+            <div className="col-md-4">
+              <img src={Truck} style={{ height: '200px', width: '300px', border: '4px solid #9dc83e', boxShadow: '2px 2px 5px #000', margin: '7px' }}></img>
+            </div>
+            <div className="col-md-1">
+              <img src={Customer} style={{ height: '200px', width: '200px', border: '4px solid #9dc83e', boxShadow: '2px 2px 5px #000', margin: '7px' }}></img>
+            </div>
+          </div><br></br><br></br><br></br>
+          <div className="row">
+            <div className="col-md-2"></div>
+            <div className='col-sm-4'>
+              <a href="https://www.instagram.com/jimmyzmobile/" target="_blank" style={{ fontSize: '28px', color: 'black', fontWeight: 'bolder' }}><i class="fab fa-instagram"></i> Jimmy'z Instagram</a>
+            </div>
+            <div className='col-sm-4'>
+              <a href="https://www.facebook.com/Jimmyzmobile/?__tn__=%2Cd%2CP-R&eid=ARB9V5muUzzQoyNhuT6NFtPH-VeS-59cHLp2wUOrgT4sMtEaykn6DOPp5YYFMYwuibzmIs-5-UeQ8xiN" 
+              target="_blank" style={{ fontSize: '28px', color: 'black', fontWeight: 'bolder' }}><i class="fab fa-facebook-square"></i> Jimmy'z Facebook</a>
+            </div>
+            <div className="col-md-2"></div>
           </div>
-        </div> */}
+          <hr></hr><br></br><br></br>
+          <div className="row">
+            <h2>Let's Schedule an Appointment!</h2>
+          </div>
+        </div>
+
         <div id="schedule_form">
           <div
             className="calendly-inline-widget"
